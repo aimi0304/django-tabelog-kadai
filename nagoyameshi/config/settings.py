a5f9dce600d48e00b942a1d80510e5abd58c27dd
aimi0304 <aimi.suzuki@outlook.jp>
2025-01-16 03:40:45 +0900

    last

--- nagoyameshi/config/settings.py
+++ nagoyameshi/config/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'syc6*!g3^07jankkb%55qn$&a4s%lp8o56ftvg!96i9y64g22d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = [os.environ.get("HOST")]
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("HOST")]
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 # Application definition
